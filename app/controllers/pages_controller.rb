a313e5417644af145dd414c5373faa76dbbf5241
Yusuke Tomita Huuskonen <yusuke.g.tomita@gmail.com>
2024-03-20 17:51:24 +0100

    search algorithm done but needs testing, search parameters passed to search_results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,5 +18,21 @@ class PagesController < ApplicationController
   end
 
   def search_results
+    raise
+    @preference = search_preference
+    @filtered_recipes = Recipe.filter_with_preference(@preference)
+  end
+
+  private
+
+  def search_preference
+    preference = {}
+    preference[:appliances] = params[:search][:appliances] if params[:search][:appliances].present?
+    preference[:ingredients] = params[:search][:ingredients] if params[:search][:ingredients].present?
+    preference[:diet] = params[:search][:diet] if params[:search][:diet].present?
+    preference[:allergies] = params[:search][:allergies] if params[:search][:allergies].present?
+    preference[:level] = params[:search][:level] if params[:search][:level].present?
+    preference[:duration] = params[:search][:duration] if params[:search][:duration].present?
+    preference
   end
 end
