4dfa3d804e348decb7fa1d25a7f85dd52ff5fc34
Joao Crespo <joao.crespo@live.com>
2024-03-20 15:11:16 +0100

    Working on the Edit Buttons now

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'search', to: 'pages#search'
   get 'profile', to: 'pages#profile'
+  patch 'profile', to: 'pages#update_profile', as: 'update_profile'
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :recipes, only: [:index, :show] do
