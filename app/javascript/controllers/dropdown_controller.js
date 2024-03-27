11a52ac780792a2d571ccdd15ebc90da3a0ada06
Yusuke Tomita Huuskonen <yusuke.g.tomita@gmail.com>
2024-03-27 16:12:06 +0100

    Dropdown JS for steps and reviews

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,12 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   connect() {
-    consolejson.log('Connected to dropdown controller')
   }
 
   toggle(event) {
-    console.log(event.target);
+    event.preventDefault();
     event.target.classList.toggle('custom-btn-active');
     event.target.classList.toggle('custom-btn');
+    event.target.closest('div').querySelector('.recipe-dropdown').classList.toggle('d-none');
   }
 }
